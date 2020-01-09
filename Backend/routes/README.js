54816167d621cb6ffc995bb98629ac3f31130abf
Mathias Bramstorp <50810755+Bramstorp@users.noreply.github.com>
2020-01-09 16:14:43 +0100

    Add files via upload

---
+++ Backend/routes/README.js
@@ -0,0 +1,3 @@
+//everything in the routes is the location on the website where the specific thing are called 
+//and if it a post where it will post to the database, if it a get where it request something like a menu layout 
+//put is used when it update something where it take something and then send it back again and delete will just remove something from the database
